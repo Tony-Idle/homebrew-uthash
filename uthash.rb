7e54f86e54a031cef1f77395e8d2b33cadf95c38
Tony <tonychen950529@gmail.com>
2022-04-24 21:58:39 +0800

    add ruby file for uthash

---
+++ uthash.rb
@@ -0,0 +1,10 @@
+require 'formula'
+
+class Uthash < Formula
+  homepage 'https://github.com/troydhanson/uthash'
+  head 'https://github.com/troydhanson/uthash.git'
+
+  def install
+    include.install Dir['src/*']
+  end
+end
